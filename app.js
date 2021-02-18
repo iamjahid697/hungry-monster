ba104b1c3b52eb6300ac82dbf0e0bc47bd5c576f
Md Jahidul Islam <iamjahid697@gmail.com>
2021-02-19 01:37:19 +0600

    all done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const findFoods = () => {
     document.getElementById('error-message').innerText = "";
 
-    const searchFood = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFood}`
+    const userInput = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayFoods(data.meals))
@@ -12,10 +12,7 @@ const displayFoods = foods => {
     const foodsDiv = document.getElementById('foods');
     foodsDiv.innerHTML = "";
     foods.forEach(food => {
-        // console.log(food);
         const foodDiv = document.createElement('div');
-
-        foodDiv.className = 'col mt-5'
         foodDiv.innerHTML = `
         <div onclick="getFood('${food.idMeal}')"  class="text-center shadow">
             <div  class="card-body css-style-card">
@@ -32,19 +29,19 @@ const getFood = idMeal => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayFoodInfo(data[1]));
+        .then(data => displayFoodInfo(data));
 }
 
 const displayFoodInfo = food => {
     console.log(food);
-    const foodDiv = document.getElementById('food-details');
+    const foodDetailDiv = document.getElementById('food-details');
     const foodDetails = `
      <h1>${food.strMeal}</h1>
      <h3>${food.strDrinkAlternate}</h3>
      <h3>${food.strCategory}</h3>
      <h3>${food.strArea}</h3>
-     `
-    foodDiv.innerHTML = foodDetails;
+     `;
+    foodDetailDiv.innerHTML = foodDetails;
 }
 
 
